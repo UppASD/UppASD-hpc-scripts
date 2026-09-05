e217e16196778e03ec29fc23cdedf3af74dc1951
Johan Hellsvik <hellsvik@kth.se>
2026-09-06 00:12:13 +0300

    Added LUMI build and job scripts

---
+++ LUMI_AMD_CPU/job_UppASD_LUMI_AMD_CPU.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -A <project>          # Set the allocation to be charged for this job
+#SBATCH -J uppasd             # The name of the script is myjob
+#SBATCH -t 24:00:00           # 24 hours wall-clock time
+#SBATCH -p standard           # The partition
+#SBATCH -n 1                  # Number of tasks
+#SBATCH -c 16                 # Number of cpus per task
+
+export SD_BINARY=<UppASD executable>
+export OMP_NUM_THREADS=16
+
+echo "Script initiated at `date` on `hostname`"
+
+time -p $SD_BINARY > out.log
+
+echo "Script finished at `date` on `hostname`"
